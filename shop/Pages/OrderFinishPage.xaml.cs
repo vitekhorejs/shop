7061437878221d357437748b7d4fc80313d80b96
Vítek Horejš <hor.vit@outlook.cz>
2017-04-26 10:14:06 +0200

    Přidejte soubory projektu.

---
+++ shop/Pages/OrderFinishPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace shop
+{
+    /// <summary>
+    /// Interakční logika pro OrderFinishPage.xaml
+    /// </summary>
+    public partial class OrderFinishPage : Page
+    {
+        public OrderFinishPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
