acd113f9ff77d2deedc4a12980842bc164d12be5
Vítek Horejš <hor.vit@outlook.cz>
2017-04-26 11:25:21 +0200

    \sdfWDE

--- shop/User.cs
+++ shop/User.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace shop
 {
-    class User
+    public class User
     {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PasswordHash { get; set; }
+        public List<Order> Orders { get; set; }
+
+
     }
 }
